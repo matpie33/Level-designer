d14de3dfda9c0903173679740cc146a9a02e9cb7
matpie33 <Spaaw@poczta.fm>
2020-04-15 23:05:45 +0200

    Add crosshair.

--- src/LevelEditor.java
+++ src/LevelEditor.java
@@ -1,4 +1,5 @@
 import com.jme3.app.SimpleApplication;
+import com.jme3.font.BitmapText;
 import com.jme3.light.AmbientLight;
 import com.jme3.math.ColorRGBA;
 import com.jme3.scene.Spatial;
@@ -33,6 +34,7 @@ public class LevelEditor extends SimpleApplication {
 		addLight();
 		ModelToSceneAdder modelToSceneAdder = new ModelToSceneAdder(rootNode);
 		modelToSceneAdder.addSpatials(spatials);
+		initCrossHairs();
 	}
 
 	private void addLight() {
@@ -40,4 +42,16 @@ public class LevelEditor extends SimpleApplication {
 		sun.setColor(ColorRGBA.White);
 		rootNode.addLight(sun);
 	}
+
+	protected void initCrossHairs() {
+		guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
+		BitmapText ch = new BitmapText(guiFont, false);
+		ch.setSize(guiFont.getCharSet().getRenderedSize() * 2);
+		ch.setText("+"); // crosshairs
+		ch.setLocalTranslation( // center
+				settings.getWidth() / 2 - ch.getLineWidth()/2,
+				settings.getHeight() / 2 + ch.getLineHeight()/2, 0);
+		guiNode.attachChild(ch);
+	}
+
 }
